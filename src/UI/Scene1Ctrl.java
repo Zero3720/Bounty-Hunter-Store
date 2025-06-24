a45999827b618e7e1f1fc1fa05114b0b5a49853b
Zero-user <77414062+Zero3720@users.noreply.github.com>
2025-06-24 12:02:27 -0300

    update11

--- src/UI/Scene1Ctrl.java
+++ src/UI/Scene1Ctrl.java
@@ -2,10 +2,48 @@ package UI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class Scene1Ctrl {
+
+    @FXML
+    private TableColumn<?, ?> BlstrCapacityClmn;
+
+    @FXML
+    private TableColumn<?, ?> BlstrColorClmn;
+
+    @FXML
+    private TableColumn<?, ?> BlstrManufcturerClmn;
+
+    @FXML
+    private TableColumn<?, ?> BlstrNameClmn;
+
+    @FXML
+    private TableColumn<?, ?> BlstrPriceClmn;
+
+    @FXML
+    private TableColumn<?, ?> BlstrRangeClmn;
+
+    @FXML
+    private Button BuyButtom;
+
+    @FXML
+    private TableColumn<?, ?> EquipDescriptionClmn;
+
+    @FXML
+    private TableColumn<?, ?> EquipManufacturerClmn;
+
+    @FXML
+    private TableColumn<?, ?> EquipNameClmn;
+
+    @FXML
+    private TableColumn<?, ?> EquipPriceClmn;
+
+    @FXML
+    private TableColumn<?, ?> EquipQuantityClmn;
 
     @FXML
     private TableView<?> EquipmentTable;
@@ -14,7 +52,22 @@ public class Scene1Ctrl {
     private TableView<?> EquipmentTable2;
 
     @FXML
+    private Button LoanButtom;
+
+    @FXML
     private CheckMenuItem MusicToggle;
+
+    private String link = "https://github.com/Zero3720/Bounty-Hunter-Store";
+
+    @FXML
+    void Buy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Loan(ActionEvent event) {
+
+    }
 
     @FXML
     void MusicToggle(ActionEvent event) {
